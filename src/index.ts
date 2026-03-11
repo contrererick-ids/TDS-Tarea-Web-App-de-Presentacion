d7d0a4c88a7c662234cd56ef0289d2b66a9d64ec
contrererick-ids <is722185@iteso.mx>
2026-03-10 21:31:11 -0600

    Implement main view of the app index.html

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import express from 'express';
+import path from 'path';
 
 const port = process.env.PORT || 3000;
 const app = express();
 
+const rutaAssets = path.join(__dirname, '..', 'public');
+app.use('/assets', express.static(rutaAssets));
+
 app.listen(3000, () => {
     console.log('api is running in port' + port)
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
