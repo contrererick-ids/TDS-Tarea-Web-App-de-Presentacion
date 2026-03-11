9885ae39833a8b6f72a7cf483be05e98afdd9172
contrererick-ids <is722185@iteso.mx>
2026-03-11 00:14:17 -0600

    Implement contact form

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,24 @@ const app = express();
 const rutaAssets = path.join(__dirname, '..', 'public');
 app.use('/assets', express.static(rutaAssets));
 
-app.listen(3000, () => {
-    console.log('api is running in port' + port)
-});
-
+// Ruta principal, sirve el index.html (SPA)
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+// Ruta al formulario de contacto
+app.get('/contacto', (req, res) => {
+    res.redirect('/#contact');
+});
+
+app.get('/thanks.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'thanks.html'));
+});
+
+app.post('/contacto', (req, res) => { 
+    res.sendFile(path.join(__dirname, 'views', 'thanks.html'));
+});
+
+app.listen(3000, () => {
+    console.log('api is running in port' + port)
 });
